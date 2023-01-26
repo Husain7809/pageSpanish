dea92f30b8b0bda3fd52a1ab3d208c2ab2b2a86b
Mahammad Husain <109744979+Husain7809@users.noreply.github.com>
2023-01-26 13:45:50 +0530

    new code

--- asset/js/app.js
+++ asset/js/app.js
@@ -3,3 +3,10 @@ function close_page()
     const card=document.querySelector('.card-1');
     card.style.display="none";
 }
+
+
+function fourPageClose()
+{
+    const card4=document.querySelector('.card-4');
+    card4.style.display="none";
+}
